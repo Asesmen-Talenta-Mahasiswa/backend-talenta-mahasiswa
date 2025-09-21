a0f0f8a5d013320b6be2436014b6abef78f40a07
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-09-21 10:11:55 +0700

    fix: db failing on prod

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,6 +4,11 @@ import * as schema from "./schema";
 
 const client = postgres(process.env.DB_URL, {
   prepare: false,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl:
     process.env.NODE_ENV === "production" ? { ca: process.env.DB_CERT } : false,
 });
